470570d4a03a4baff2f73db2f2ef32dd4ef08d4b
rjrahul98 <rjrahul1711@gmail.com>
2019-09-19 14:45:18 +0530

    navbar implemented

--- testPortalWeb/src/app/app.module.ts
+++ testPortalWeb/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { RegistrationComponent } from './component/registration/registration.component';
+import { AboutComponent } from './component/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    FooterComponent,
+    RegistrationComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
