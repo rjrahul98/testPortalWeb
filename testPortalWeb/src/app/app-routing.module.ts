fdbde76766af47f04737502d5f71678723094f3f
rjrahul98 <rjrahul1711@gmail.com>
2019-09-22 02:02:09 +0530

    login completed and footer added

--- testPortalWeb/src/app/app-routing.module.ts
+++ testPortalWeb/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { RegistrationComponent } from './component/registration/registration.component';
+import { AboutComponent } from './component/about/about.component';
+import { LoginComponent } from './component/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:HomeComponent},
+  {path: 'register', component:RegistrationComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'login', component:LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
